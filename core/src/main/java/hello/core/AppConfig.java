8dcc30994ff56adb44001f8551cc2d6d114f0c53
Heekyeong Park <qkrgmlrud00@gmail.com>
2022-11-17 19:26:32 +0900

    feat: spring 적용

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -9,27 +9,33 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-
+@Configuration
 public class AppConfig {
     // 클라이언트 코드 memberService, OrderService 인터페이스의 구현 객체 생성
     // 생성자를 통한 주입 (DI)
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy());
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
     // 할인 정책 변경 코드 수정 시 여기만 수정하면 됨 (객체 지향 설계)
-    private DiscountPolicy discountPolicy() {
+    @Bean
+    public DiscountPolicy discountPolicy() {
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
     }
--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -1,13 +1,25 @@
 package hello.core;
 
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class MemberApp {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MemberApp.class, args);
-	}
+		ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+		MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
+		Member member = new Member(1L, "memberA", Grade.VIP);
+		memberService.join(member);
+		Member findMember = memberService.findMember(1L);
+		System.out.println("new member = " + member.getName());
+		System.out.println("find Member = " + findMember.getName());
+
+	}
 }
